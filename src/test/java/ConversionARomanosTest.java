a2ec383a6cb90339cbdae6c406be14e9579d70fc
JorgeDiezGracia <133242180+JorgeDiezGracia@users.noreply.github.com>
2023-08-29 16:46:43 +0200

    Paso test24, provoco error, después correcto

--- src/test/java/ConversionARomanosTest.java
+++ src/test/java/ConversionARomanosTest.java
@@ -77,7 +77,11 @@ public class ConversionARomanosTest {
         numero = conversionARomanos.conversionNumero(16);
         Assert.assertEquals("XVI",numero);
     }
-
+    @Test
+    public void testConverter24(){
+        numero = conversionARomanos.conversionNumero(24);
+        Assert.assertEquals("XXIV", numero);
+    }
 
 
 
