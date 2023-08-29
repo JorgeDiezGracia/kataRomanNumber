f5281a1ee8d5a2bd4d795ba2c05c0ec2a8498579
JorgeDiezGracia <133242180+JorgeDiezGracia@users.noreply.github.com>
2023-08-29 13:43:53 +0200

    Realizo primer test, primero provoco error, después correcto

--- src/main/java/com/svalero/Main.java
+++ src/main/java/com/svalero/Main.java
@@ -1,7 +1,14 @@
 package com.svalero;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un número del 1 al 3000:");
+        int num = Integer.parseInt(teclado.nextLine());
+
+        ConversionARomanos conversor = new ConversionARomanos();
+        System.out.println(conversor.conversionNumero(num));
     }
 }
---
+++ src/test/java/ConversionARomanosTest.java
@@ -0,0 +1,23 @@
+import com.svalero.ConversionARomanos;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ConversionARomanosTest {
+    ConversionARomanos conversionARomanos;
+    String numero;
+
+    @Before
+    public void init() {
+        conversionARomanos = new ConversionARomanos();
+        numero = "";
+    }
+
+    @Test
+    public void testConverter() {
+        numero = conversionARomanos.conversionNumero(1);
+        Assert.assertEquals("I", numero);
+    }
+    }
+
